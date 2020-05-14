73a20fe84da5d6c2275ca877804e8977755521d2
Akira <matyapiro31@users.noreply.github.com>
2020-05-14 13:24:20 +0900

    Fix conflict.
    1. add organization to comments.
    2. fix conflict.

--- tlds_custom.go
+++ tlds_custom.go
@@ -3,25 +3,25 @@ package isdomain
 // ExtendedTLDs is a set of additional "TLDs", allowing decentralized name
 // systems, like TOR and Namecoin.
 var ExtendedTLDs = map[string]bool{
-	"BIT":   true,
-	"ONION": true,
-	"ETH":   true,
-	"BBS":   true,
-	"CHAN":  true,
-	"CYB":   true,
-	"DYN":   true,
-	"EPIC":  true,
-	"GEEK":  true,
-	"GOPHER":true,
-	"INDY":  true,
-	"LIBRE": true,
-	"NEO":   true,
-	"NULL":  true,
-	"O":     true,
-	"OSS":   true,
-	"OZ":    true,
-	"PARODY":true,
-	"PIRATE":true,
+	"BIT":    true, // namecoin.org
+	"ONION":  true, // torproject.org
+	"ETH":    true, // ens.domains
 	"CRYPTO": true, // unstoppabledomains.com
 	"ZIL":    true, // unstoppabledomains.com
+	"BBS":    true, // opennic.org
+	"CHAN":   true, // opennic.org
+	"CYB":    true, // opennic.org
+	"DYN":    true, // opennic.org
+	"EPIC":   true, // opennic.org
+	"GEEK":   true, // opennic.org
+	"GOPHER": true, // opennic.org
+	"INDY":   true, // opennic.org
+	"LIBRE":  true, // opennic.org
+	"NEO":    true, // opennic.org
+	"NULL":   true, // opennic.org
+	"O":      true, // opennic.org
+	"OSS":    true, // opennic.org
+	"OZ":     true, // opennic.org
+	"PARODY": true, // opennic.org
+	"PIRATE": true, // opennic.org
 }
